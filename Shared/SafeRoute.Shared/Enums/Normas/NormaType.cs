e7938704f2286028824dbd93dc445709f29ba5ab
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2026-01-11 11:16:38 -0300

    feat: add norma type enum to shared

---
+++ Shared/SafeRoute.Shared/Enums/Normas/NormaType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeRoute.Shared.Enums.Normas
+{
+    public enum NormaType
+    {
+        [Display(Name = "Undefined")]
+        Undefined = 0,
+
+        [Display(Name = "NBR 9050")]
+        NBR_9050 = 1,
+
+        [Display(Name = "NBR 9077")]
+        NBR_9077 = 2
+    }
+}
