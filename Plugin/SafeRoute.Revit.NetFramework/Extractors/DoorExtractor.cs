40726d724113198c1b54c93fccc3a084dfe52297
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2026-01-11 21:13:58 -0300

    feat: add plugin orchestration service for model ingestion

---
+++ Plugin/SafeRoute.Revit.NetFramework/Extractors/DoorExtractor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using SafeRoute.Revit.NetFramework.Models;
+using SafeRoute.Revit.NetFramework.Utils;
+
+namespace SafeRoute.Revit.NetFramework.Extractors
+{
+    public class DoorExtractor
+    {
+        private readonly Document _document;
+
+        public DoorExtractor(Document document)
+        {
+            _document = document;
+        }
+
+        public IList<DoorData> Extract()
+        {
+            var result = new List<DoorData>();
+
+            var collector = new FilteredElementCollector(_document)
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .WhereElementIsNotElementType();
+
+            foreach (var element in collector)
+            {
+                var door = element as FamilyInstance;
+                if (door == null)
+                    continue;
+
+                var widthParam = door.get_Parameter(BuiltInParameter.DOOR_WIDTH);
+                var heightParam = door.get_Parameter(BuiltInParameter.DOOR_HEIGHT);
+
+                if (widthParam == null || heightParam == null)
+                    continue;
+
+                var doorData = new DoorData
+                {
+                    ElementId = door.Id.IntegerValue.ToString(),
+                    Width = UnitConverter.FeetToMeters(widthParam.AsDouble()),
+                    Height = UnitConverter.FeetToMeters(heightParam.AsDouble()),
+                    IsAccessible = false // Fase 1
+                };
+
+                result.Add(doorData);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Plugin/SafeRoute.Revit.NetFramework/Services/ModelIngestionService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using SafeRoute.Revit.NetFramework.Adapters;
+using SafeRoute.Revit.NetFramework.Extractors;
+using SafeRoute.Shared.Dtos.Ingestion.Doors;
+using SafeRoute.Shared.Dtos.Ingestion.Ramps;
+using SafeRoute.Shared.Enums.Normas;
+
+namespace SafeRoute.Revit.NetFramework.Services
+{
+    public class ModelIngestionService
+    {
+        private readonly Document _document;
+
+        public ModelIngestionService(Document document)
+        {
+            _document = document;
+        }
+
+        public IList<DoorIngestionDto> GetDoors(NormaTypeEnum norma)
+        {
+            var extractor = new DoorExtractor(_document);
+            var doorModels = extractor.Extract();
+
+            var result = new List<DoorIngestionDto>();
+            foreach (var door in doorModels)
+            {
+                result.Add(DoorAdapter.ToIngestionDto(door, norma));
+            }
+
+            return result;
+        }
+
+        public IList<RampIngestionDto> GetRamps(NormaTypeEnum norma)
+        {
+            var extractor = new RampExtractor(_document);
+            var rampModels = extractor.Extract();
+
+            var result = new List<RampIngestionDto>();
+            foreach (var ramp in rampModels)
+            {
+                result.Add(RampAdapter.ToIngestionDto(ramp, norma));
+            }
+
+            return result;
+        }
+    }
+}
