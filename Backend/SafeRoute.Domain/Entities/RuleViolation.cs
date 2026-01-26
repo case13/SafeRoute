b0fb71baa31e961b32468fedbdccc5eec3418a29
DESKTOP-8106DVP\User <anderson.case13@gmail.com>
2026-01-26 09:49:43 -0300

    feat: add rule violation domain entity and severity enum

---
+++ Backend/SafeRoute.Domain/Entities/RuleViolation.cs
@@ -0,0 +1,18 @@
+﻿using SafeRoute.Shared.Enums.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeRoute.Domain.Entities
+{
+    public class RuleViolation : BaseEntity
+    {
+        public string ElementExternalId { get; set; } = default!;
+        public string ElementType { get; set; } = default!;
+        public string RuleCode { get; set; } = default!;
+        public string Message { get; set; } = default!;
+        public RuleSeverityEnum Severity { get; set; }
+    }
+}
---
+++ Shared/SafeRoute.Shared/Dtos/Rules/RuleSeverityEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SafeRoute.Shared.Dtos.Rules
+{
+    public enum RuleSeverityEnum
+    {
+        [Display(Name = "Informational")]
+        Info = 1,
+        [Display(Name = "Warning")]
+        Warning = 2,
+        [Display(Name = "Error")]
+        Error = 3
+    }
+}
